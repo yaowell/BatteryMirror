e181f388c81049a3713f405e529b716b973f337b
yaowell <280442219@qq.com>
2026-09-02 18:25:25 +0800

    Delete prefs directory
    删除设置项 做精简

--- prefs/BMPRootListController.h
+++
@@ -1,7 +0,0 @@
-#import <Preferences/PSListController.h>
-#import <Preferences/PSSpecifier.h>
-
-@interface BMPRootListController : PSListController
-- (id)readPreferenceValue:(PSSpecifier *)specifier;
-- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier;
-@end
